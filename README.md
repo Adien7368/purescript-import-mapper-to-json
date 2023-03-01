9ed0ab38899419a92dec208ab999b8f7498c06f8
Adien7368 <avihsan8922@gmail.com>
2023-03-01 21:10:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# purescript-import-mapper-to-json
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-88sjv8)
---
+++ src/main.ts
@@ -0,0 +1,28 @@
+let input = <HTMLInputElement>document.getElementById('input1');
+
+input.oninput = (e) => {
+  let value = input.value;
+  let arr = value.split('\n');
+  let ans: Map<string, Array<string>> = new Map();
+
+  for (let i = 0; i < arr.length; ++i) {
+    if (arr[i].split(':')[1].trim().substring(0, 6) == 'module') {
+      let module = arr[i].split(':')[1].trim().substring(6).trim();
+      let j = i + 1;
+      let children = [];
+      while (
+        j < arr.length &&
+        arr[j].split(':')[1].trim().substring(0, 6) == 'import'
+      ) {
+        children.push(arr[j].split(':')[1].trim().substring(6).trim());
+        j++;
+      }
+      ans.set(module, children);
+    }
+  }
+  let output = <HTMLInputElement>document.getElementById('output1');
+  console.log(Object.fromEntries(ans));
+  output.value = JSON.stringify(Object.fromEntries(ans));
+};
+
+export {};
